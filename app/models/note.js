19f370fb32d9526f695ee5f46594bb6072755e1b
João Paulo Dubas <joao.dubas@gmail.com>
2014-02-08 13:57:56 -0200

    [fix] Implement some missing methods from Note model.

--- app/models/note.js
+++ app/models/note.js
@@ -1,47 +1,130 @@
 'use strict';
+var slug = require('slug');
 var db = require('./db.js');
+
+module.exports = exports = Note;
 
 /**
  * Represents a note instace.
  */
-function Note(note) {
+function Note(details) {
   if (!(this instanceof Note)) {
     return new Note(note);
   }
 
-  this.note = note;
-  this.complete = false;
-  this._slug = null;
+  this.note = details['note'];
+  this.complete = details['complete'] || false;
+  this.slug = details['slug'] || slug(this.note);
+  this._id = this.constructor.setId(this.slug);
 }
 
 Note._key = db._makeKey(Note);
 Note.all = all;
 Note.create = create;
-Note.completeAll = completeAll;
-Note.prototype.complete = complete;
-Note.prototype.remove = remove;
+Note.remove = remove;
+Note.setId = setId;
+Note.prototype.remove = removeInstance;
+
+/**
+ * Convert a note instance into a JSON string.
+ * @param {Note} note: instance to be stringified
+ * @return {String} the instance as a JSON string.
+ */
+function toJSON(note) {
+    return JSON.stringify({
+      "note": note.note,
+      "complete": note.complete,
+      "slug": note.slug
+    });
+}
+
+function toNote(details) {
+  console.log(details);
+  console.log(JSON.parse(details));
+  return new Note(JSON.parse(details));
+}
 
 /**
  * Retrieve all notes available in the database
+ *
+ * @return {Stream}: a readable stream with objectMode set to true
  */
-function all() {}
+function all() {
+  return db.createRpcStream();
+}
 
 /**
  * Create a new note.
+ *
+ * @param {Object} details: an object with the note details, the following keys
+ * are expected:
+ *     * {String} note
+ *     * {Boolean} complete
+ *     * {String} slug [optional]
+ * @param {Function} fn: callback to be executed after the note is persisted,
+ * receives an error || null and the note instance.
  */
-function create() {}
+function create(details, fn) {
+  var note = new this(details);
+  db.put(
+    note._id,
+    toJSON(note),
+    function (err) {
+      fn(err, note);
+    }
+  );
+}
+
+function remove(slug, fn) {
+  var _id = this.setId(slug);
+  console.log('id', _id);
+  db.get(_id, function (err, data) {
+    var details = toNote(data);
+    db.del(_id, function (err) {
+      console.log(err);
+      fn(err, details);
+    });
+  });
+}
 
 /**
- * Set all notes as completed.
+ * Change the complete status of a note instance.
+ *
+ * @param {Function} fn: callback to be execute after the note is persisted,
+ * receives an error || null and the note instance.
  */
-function completeAll() {}
+function change(fn) {
+  var self = this;
+  this.complete = !this.complete;
+  db.put(
+    this._id,
+    toJSON(this),
+    function (err) {
+      fn(err, self);
+    }
+  );
 
-/**
- * Set a note instance to complete.
- */
-function complete() {}
+}
 
 /**
  * Remove a note instance.
+ *
+ * @param {Function} fn: callback to be execute after the note is removed,
+ * receives an error || null and the note instance.
  */
-function remove() {}
+function removeInstance(fn) {
+  var self = this;
+  db.del(
+    this._id,
+    function (err) {
+      fn(err, self);
+    }
+  );
+}
+
+/**
+ * Define the id attribute for a given instance.
+ */
+function setId(slug) {
+  return this._key + slug;
+}
