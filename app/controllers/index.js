3a9e9008aefae4cc3369b6984a0e082f3fe6310b
João Paulo Dubas <joao.dubas@gmail.com>
2014-02-08 13:58:47 -0200

    [add] Methods to create/remove notes.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,35 +3,47 @@ var Note = require('../models').Note;
 
 exports.list = list;
 exports.create = create;
-exports.update = update;
+exports.done = done;
 exports.remove = remove;
+
+/**
+ * Convert a stream of objects into a stream of Bytes
+ */
+function toBuffer() {}
 
 /**
  * List note instances
  */
-function list(req, resp, next) {}
-
-/**
- * Filter note instances
- */
-function filter(req, resp, next) {}
+function list(req, res, next) {}
 
 /**
  * Create a note instance.
  */
-function create(req, resp, next) {}
-
-/**
- * Mark all notes as done.
- */
-function allDone(req, resp, next) {}
+function create(req, res, next) {
+  var details = {
+    note: req.body.note
+  }
+  Note.create(details, function (err, note) {
+    res.render(
+      'partial/note',
+      {note: note.note, slug: note.slug}
+    );
+  });
+}
 
 /**
  * Mark note instance as done.
  */
-function done(req, resp, next) {}
+function done(req, res, next) {}
 
 /**
  * Remove a note instance.
  */
-function remove(req, resp, next) {}
+function remove(req, res, next) {
+  Note.remove(req.params.slug, function (err, note) {
+    res.render(
+      'partial/message',
+      {message: 'Nota: <strong>' + note.note + '</strong> removida com sucesso!'}
+    );
+  });
+}
