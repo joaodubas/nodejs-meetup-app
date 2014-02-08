1687d14cc0c9e3a2e3a5ecedde0af322280bacbe
João Paulo Dubas <joao.dubas@gmail.com>
2014-02-08 13:56:50 -0200

    [fix] Connection to leveldb instance.

--- app/models/db.js
+++ app/models/db.js
@@ -1,6 +1,6 @@
 'use strict';
 var net = require('net');
-var multilevel = require('multileve');
+var multilevel = require('multilevel');
 
 var db = multilevel.client();
 db._separator = '~';
@@ -9,7 +9,12 @@ db._rangeUpperLimit = '~';
 db._makeKey = makeKey;
 
 
-var conn = net.connect(parseInt(process.env.DB_PORT_3001_TCP_PORT));
+var conn = net.connect({
+  port: parseInt(process.env.DB_PORT_3001_TCP_PORT),
+  host: process.env.DB_PORT_3001_TCP_ADDR
+});
+
+console.log('Connect to multilevel through ' + process.env.DB_PORT_3001_TCP_ADDR + ':' + process.env.DB_PORT_3001_TCP_PORT);
 conn.pipe(db.createRpcStream()).pipe(conn);
 
 module.exports = exports = db;
