4c759b057f0202532fef6c37c9d061c09675694f
João Paulo Dubas <joao.dubas@gmail.com>
2014-02-13 16:08:17 -0200

    [create] Transform stream to render database values.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,11 @@
 'use strict';
+var fs = require('fs');
+var path = require('path');
+var stream = require('stream');
+var util = require('util');
+
+var jade = require('jade');
+
 var Note = require('../models').Note;
 
 exports.list = list;
@@ -9,12 +16,43 @@ exports.remove = remove;
 /**
  * Convert a stream of objects into a stream of Bytes
  */
-function toBuffer() {}
+var TEMPLATE = '';
+(function () {
+  var root = path.join(process.cwd(), 'views', 'partial', 'note.jade');
+  var options = {'encoding': 'utf-8'};
+  fs.readFile(root, options, function (err, content) {
+    if (err) {
+      throw err;
+    }
+    TEMPLATE = jade.compile(content);
+  });
+})();
+util.inherits(RenderStream, stream.Transform);
+function RenderStream(options) {
+  if (!this instanceof RenderStream) {
+    return new Render(options);
+  }
+  stream.Transform.call(this, options);
+  this._writableState.objectMode = true;
+  this._readableState.objectMode = false;
+
+}
+RenderStream.prototype._transform = function _transform(chunk, encoding, callback) {
+  this.push(TEMPLATE(chunk.value));
+  callback();
+};
+RenderStream.prototype._flush = function _flush(callback) {
+  callback();
+};
 
 /**
  * List note instances
  */
-function list(req, res, next) {}
+function list(req, res, next) {
+  var stream = Note.all();
+  var transform = new RenderStream();
+  stream.pipe(transform).pipe(res);
+}
 
 /**
  * Create a note instance.
